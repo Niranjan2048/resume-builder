b92de5c4b8e72ae5067cffc1760401525066e0d9
unknown <lokhanderobal19ce@student.mes.ac.in>
2021-04-19 11:16:07 +0530

    changed

---
+++ script.js
@@ -0,0 +1,33 @@
+function addNewAQField(){
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("row",3);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let aq0b = document.getElementsById("aq");
+    let aqAddButton0b = document.getElementsById("aqAddButton");
+
+    aq0b.insertBefore(newNode,aqAddButton0b);
+}
+
+//CV generation
+function generateCV(){
+    document.getElementById('nameT1').innerHTML = document.getElementById('nameField').value;
+
+    //document.getElementById('nameT2').innerHTML = nameField;
+
+    //contact
+    document.getElementById('contactT').innerHTML = document.getElementById('conactField').value;
+
+    //address
+    document.getElementById('addressT').innerHTML = document.getElementById('addressField').value;
+
+    //facebook
+    //document.getElementById('fbT').innerHTML = document.getElementById('fbField').value;
+
+    document.getElementById('instaT').innerHTML = document.getElementById('instaField').value;
+
+    document.getElementById('linkedT').innerHTML = document.getElementById('linkedField').value;
+}
