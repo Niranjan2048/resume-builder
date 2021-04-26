d18aa78ad25f5ab00d961a2c009e6910fe3d99cd
unknown <lokhanderobal19ce@student.mes.ac.in>
2021-04-26 17:24:04 +0530

    Added name tag 2

---
+++ template.js
@@ -0,0 +1,13 @@
+document.getElementById("nameT1").innerHTML = localStorage.getItem("name");
+document.getElementById("nameT2").innerHTML = localStorage.getItem("name");
+document.getElementById("contactT").innerHTML = localStorage.getItem("contact");
+document.getElementById("addressT").innerHTML = localStorage.getItem("address");
+document.getElementById("mailT").innerHTML = localStorage.getItem("mail");
+//document.getElementById("fbT").innerHTML = localStorage.getItem("");
+document.getElementById("instaT").innerHTML = localStorage.getItem("insta");
+document.getElementById("linkedT").innerHTML = localStorage.getItem("linkedin");
+document.getElementById("gitT").innerHTML = localStorage.getItem("github");
+document.getElementById("objectiveT").innerHTML = localStorage.getItem("objective");
+document.getElementById("weT").innerHTML = localStorage.getItem("work");
+document.getElementById("aqT").innerHTML = localStorage.getItem("qualification");
+document.getElementById("ihT").innerHTML = localStorage.getItem("hobbie");
